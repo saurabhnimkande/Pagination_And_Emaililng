cc7c3cbaf4e69fa6c660840a334ead947d0e44bf
SaurabhN16 <91530878+saurabhnimkande@users.noreply.github.com>
2021-12-01 09:29:22 +0530

    added .env file

--- src/configs/mail.js
+++ src/configs/mail.js
@@ -1,11 +1,12 @@
 const nodemailer= require('nodemailer')
+require('dotenv').config();
 
 module.exports=nodemailer.createTransport({
     host: "smtp.mailtrap.io",
     port: 587,
     secure: false, // upgrade later with STARTTLS
     auth: {
-      user: "b518ac38354b9f",
-      pass: "b889ca02046d81",
+      user: process.env.SMTP_USERNAME,
+      pass: process.env.SMTP_PASSWORD,
     },
   });
--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const connect = require('./configs/db');
 const start= () => {
     app.listen(2121,async function () {
         await connect();
-        console.log('listening on port 2525');
+        console.log('listening on port 2121');
     });
 }
 start();
